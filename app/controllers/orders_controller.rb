7573e8b5e8402fb64bdb52847ad5c076dff5c7f2
Amgad Amer <amgadamer2001@yahoo.com>
2015-07-15 16:58:51 -0400

    Adjusted header, navbar and about page

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -27,6 +27,7 @@ class OrdersController < ApplicationController
     @order.buyer_id = current_user.id
     @order.seller_id = @seller.id
 
+    require "stripe"
     Stripe.api_key = ENV["STRIPE_API_KEY"]
     token = params[:stripeToken]
 
@@ -37,9 +38,15 @@ class OrdersController < ApplicationController
         :card => token
         )
       flash[:notice] = "Thanks for ordering!"
-    rescue Stripe::cardError => e
+    rescue Stripe::CardError => e
       flash[:danger] = e.message
     end
+
+    transfer = Stripe::Transfer.create(
+      :amount => (@listing.price * 95).floor,
+      :currency => "usd",
+      :recipient => @seller.recipient
+      )
 
     respond_to do |format|
       if @order.save
